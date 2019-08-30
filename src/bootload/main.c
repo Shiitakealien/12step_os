91c8221089520a8137ba14fcb65900faaf4cffab
Default User <user@localhost.localdomain>
2019-08-31 03:32:05 +0900

    06

--- src/bootload/main.c
+++ src/bootload/main.c
@@ -51,6 +51,8 @@ int main(void) {
     static char buf[16];
     static long size = -1;
     static unsigned char *loadbuf = NULL;
+    char *entry_point;
+    void (*f)(void);
     extern int buffer_start; /* buffer defined in linker script */
 
     init();
@@ -76,7 +78,17 @@ int main(void) {
             puts("\n");
             dump(loadbuf, size);
         } else if (!strcmp(buf, "run")) { 
-            elf_load(loadbuf);
+            entry_point = elf_load(loadbuf);
+            if (!entry_point) {
+                puts("run error!\n");
+            } else {
+                puts("starting from entry point: ");
+                putxval((unsigned long)entry_point, 0);
+                puts("\n");
+                f = (void (*)(void))entry_point;
+                f(); /* pass the process to a loadede program */
+                /* no return to here */
+            }
         } else {
             puts("unknown.\n");
         }
